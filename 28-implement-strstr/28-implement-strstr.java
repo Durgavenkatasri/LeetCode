a9f168ae44b4237d0ac4161e536835d15d39ce9f
Durga Venkatasree <105306626+Durgavenkatasri@users.noreply.github.com>
2022-07-09 18:26:14 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (79.44%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(needle.isEmpty())
+        {
+            return 0;
+        }
+        else
+        {
+            return haystack.indexOf(needle);
+         }
+        /*else
+        {
+            return -1;
+        }*/
+        
+    }
+}
